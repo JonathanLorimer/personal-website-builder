501dbbf98f0afaa9d395e29079debfb50ed8eb08
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-07-11 23:27:42 -0400

    haskell.nix

--- app/Main.hs
+++ app/Main.hs
@@ -8,10 +8,13 @@
 
 import           Control.Lens
 import           Control.Monad
-import           Data.Aeson                 as A
+import           Control.Monad.Trans.Reader
+import           Control.Monad.Trans.Class
+import           Data.Aeson
 import           Data.Aeson.Lens
 import qualified Data.HashMap.Lazy          as HML
 import           Data.List                  (sortBy)
+import           Data.Maybe
 import qualified Data.Text                  as T
 import           Data.Time
 import           Development.Shake
@@ -20,6 +23,8 @@ import           Development.Shake.FilePath
 import           Development.Shake.Forward
 import           GHC.Generics               (Generic)
 import           Slick
+import           System.Environment (lookupEnv)
+
 
 {------------------------------------------------
                     Config
@@ -35,8 +40,12 @@ siteMeta =
              , twitterUser = "jonathanlorime1"
              }
 
-outputFolder :: FilePath
-outputFolder = "docs/"
+type SiteM = ReaderT FilePath Action
+
+forP' :: [a] -> (a -> SiteM b) -> SiteM [b]
+forP' xs f = do
+  env <- ask
+  lift $ forP xs (\x -> runReaderT (f x) env)
 
 withSiteMeta :: Value -> Value
 withSiteMeta (Object obj) = Object $ HML.union obj siteMetaObj
@@ -154,74 +163,83 @@ buildEducation srcPath = cacheAction ("build" :: T.Text, srcPath) $ do
   eduData <- markdownToHTML . T.pack $ eduContent
   convert eduData
 
-buildAboutMe :: Action ()
+buildAboutMe :: SiteM ()
 buildAboutMe  = do
+  outputFolder <- ask
+  lift $ do
+    -- Get Paths
+    [bioPath] <- getDirectoryFiles "." ["site/aboutme//bio.md"]
+    experiencePaths <- getDirectoryFiles "." ["site/aboutme/experience//*.md"]
+    educationPaths <- getDirectoryFiles "." ["site/aboutme/education//*.md"]
 
-  -- Get Paths
-  [bioPath] <- getDirectoryFiles "." ["site/aboutme//bio.md"]
-  experiencePaths <- getDirectoryFiles "." ["site/aboutme/experience//*.md"]
-  educationPaths <- getDirectoryFiles "." ["site/aboutme/education//*.md"]
+    -- Build Data Structures
+    bioData <- buildBio bioPath
+    expsData <- forP experiencePaths buildExperience
+    edusData <- forP educationPaths buildEducation
+    let aboutMeData = AboutMe { bio = bioData
+                              , experience = sortBy (flip compare) expsData
+                              , education = edusData
+                              }
+    -- Compile HTML
+    aboutMeT <- compileTemplate' "site/templates/aboutme.html"
+    let cvHTML = T.unpack $ substitute aboutMeT $ withSiteMeta $ toJSON aboutMeData
+    writeFile' (outputFolder </> "aboutme.html") cvHTML
 
-  -- Build Data Structures
-  bioData <- buildBio bioPath
-  expsData <- forP experiencePaths buildExperience
-  edusData <- forP educationPaths buildEducation
-  let aboutMeData = AboutMe { bio = bioData
-                            , experience = sortBy (flip compare) expsData
-                            , education = edusData
-                            }
-  -- Compile HTML
-  aboutMeT <- compileTemplate' "site/templates/aboutme.html"
-  let cvHTML = T.unpack $ substitute aboutMeT $ withSiteMeta $ toJSON aboutMeData
-  writeFile' (outputFolder </> "aboutme.html") cvHTML
-
-buildIndex :: Action ()
+buildIndex :: SiteM ()
 buildIndex = do
-  indexT <- compileTemplate' "site/templates/index.html"
-  let indexHTML = T.unpack $ substitute indexT $ toJSON siteMeta
-  writeFile' (outputFolder </> "index.html") indexHTML
+  outputFolder <- ask
+  lift $ do
+    indexT <- compileTemplate' "site/templates/index.html"
+    let indexHTML = T.unpack $ substitute indexT $ toJSON siteMeta
+    writeFile' (outputFolder </> "index.html") indexHTML
 
 -- | given a list of posts this will build a table of contents
-buildTableOfContents :: [Post] -> Action ()
+buildTableOfContents :: [Post] -> SiteM ()
 buildTableOfContents posts' = do
-  postsT <- compileTemplate' "site/templates/posts.html"
-  let postsInfo = PostsInfo { posts = sortBy (\x y -> compare (date y) (date x)) posts' }
-      postsHTML = T.unpack $ substitute postsT (withSiteMeta $ toJSON postsInfo)
-  writeFile' (outputFolder </> "posts.html") postsHTML
+  outputFolder <- ask
+  lift $ do
+    postsT <- compileTemplate' "site/templates/posts.html"
+    let postsInfo = PostsInfo { posts = sortBy (\x y -> compare (date y) (date x)) posts' }
+        postsHTML = T.unpack $ substitute postsT (withSiteMeta $ toJSON postsInfo)
+    writeFile' (outputFolder </> "posts.html") postsHTML
 
 -- | Find and build all posts
-buildPosts :: Action [Post]
+buildPosts :: SiteM [Post]
 buildPosts = do
-  pPaths <- getDirectoryFiles "." ["site/posts//*.md"]
-  forP pPaths buildPost
+  pPaths <- lift $ getDirectoryFiles "." ["site/posts//*.md"]
+  forP' pPaths buildPost
 
 -- | Load a post, process metadata, write it to output, then return the post object
 -- Detects changes to either post content or template
-buildPost :: FilePath -> Action Post
-buildPost srcPath = cacheAction ("build" :: T.Text, srcPath) $ do
-  liftIO . putStrLn $ "Rebuilding post: " <> srcPath
-  postContent <- readFile' srcPath
-  -- load post content and metadata as JSON blob
-  postData <- markdownToHTML . T.pack $ postContent
-  let postUrl = T.pack . dropDirectory1 $ srcPath -<.> "html"
-      withPostUrl = _Object . at "url" ?~ String postUrl
-  -- Add additional metadata we've been able to compute
-  let fullPostData = withSiteMeta . withPostUrl $ postData
-  template <- compileTemplate' "site/templates/post.html"
-  writeFile' (outputFolder </> T.unpack postUrl) . T.unpack $ substitute template fullPostData
-  convert fullPostData
+buildPost :: FilePath -> SiteM Post
+buildPost srcPath = do
+  outputFolder <- ask
+  lift . cacheAction ("build" :: T.Text, srcPath) $ do
+    liftIO . putStrLn $ "Rebuilding post: " <> srcPath
+    postContent <- readFile' srcPath
+    -- load post content and metadata as JSON blob
+    postData <- markdownToHTML . T.pack $ postContent
+    let postUrl = T.pack . dropDirectory1 $ srcPath -<.> "html"
+        withPostUrl = _Object . at "url" ?~ String postUrl
+    -- Add additional metadata we've been able to compute
+    let fullPostData = withSiteMeta . withPostUrl $ postData
+    template <- compileTemplate' "site/templates/post.html"
+    writeFile' (outputFolder </> T.unpack postUrl) . T.unpack $ substitute template fullPostData
+    convert fullPostData
 
 -- | Copy all static files from the listed folders to their destination
-copyStaticFiles :: Action ()
+copyStaticFiles :: SiteM ()
 copyStaticFiles = do
-    filepaths <- getDirectoryFiles "./site/"
-      ["images//*"
-      , "css//*"
-      , "js//*"
-      , "fonts//*"
-      ]
-    void $ forP filepaths $ \filepath ->
-        copyFileChanged ("site" </> filepath) (outputFolder </> filepath)
+    outputFolder <- ask
+    lift $ do
+      filepaths <- getDirectoryFiles "./site/"
+        ["images//*"
+        , "css//*"
+        , "js//*"
+        , "fonts//*"
+        ]
+      void $ forP filepaths $ \filepath ->
+          copyFileChanged ("site" </> filepath) (outputFolder </> filepath)
 
 formatDate :: String -> String
 formatDate humanDate = toIsoDate parsedTime
@@ -235,8 +253,9 @@ rfc3339 = Just "%H:%M:%SZ"
 toIsoDate :: UTCTime -> String
 toIsoDate = formatTime defaultTimeLocale (iso8601DateFormat rfc3339)
 
-buildFeed :: [Post] -> Action ()
+buildFeed :: [Post] -> SiteM ()
 buildFeed posts = do
+  outputFolder <- ask
   now <- liftIO getCurrentTime
   let atomData =
         AtomData
@@ -247,26 +266,33 @@ buildFeed posts = do
           , currentTime = toIsoDate now
           , atomUrl = "/atom.xml"
           }
-  atomTempl <- compileTemplate' "site/templates/atom.xml"
-  writeFile' (outputFolder </> "atom.xml") . T.unpack $ substitute atomTempl (toJSON atomData)
+  lift $ do
+    atomTempl <- compileTemplate' "site/templates/atom.xml"
+    writeFile' (outputFolder </> "atom.xml") . T.unpack $ substitute atomTempl (toJSON atomData)
 
 mkAtomPost :: Post -> Post
 mkAtomPost p = p { date = formatDate $ datePretty p }
+
+buildCNAME :: SiteM ()
+buildCNAME = ask >>= \outputFolder ->
+  writeFile' (outputFolder </> "CNAME") . T.unpack $ "jonathanlorimer.dev"
 
 {------------------------------------------------
                  Shake Build
  ------------------------------------------------}
 
-buildRules :: Action ()
+buildRules :: SiteM ()
 buildRules = do
   allPosts <- buildPosts
   buildAboutMe
   buildIndex
   buildTableOfContents allPosts
   buildFeed allPosts
+  buildCNAME
   copyStaticFiles
 
 main :: IO ()
 main = do
   let shOpts = shakeOptions { shakeVerbosity = Chatty, shakeLintInside = ["\\"] }
-  shakeArgsForward shOpts buildRules
+  mOutputDir <- lookupEnv "OUTPUT_DIR"
+  shakeArgsForward shOpts $ runReaderT buildRules (fromMaybe "build/" mOutputDir)
--- nix/pinned-nixpkgs.nix
+++
@@ -1,5 +0,0 @@
-builtins.fetchGit {
-  url = "https://github.com/nixos/nixpkgs/";
-  ref = "refs/heads/nixos-unstable";
-  rev = "f5e8bdd07d1afaabf6b37afc5497b1e498b8046f";
-}
